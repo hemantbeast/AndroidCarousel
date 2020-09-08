ad111f89839fdef5ee2070d6e2d2f1177e8acd3c
Hemant Sharma <hemantbeast@gmail.com>
2020-09-08 13:12:13 +0530

    Initial Commit
     - Reproduction of bug in carousel view

---
+++ AndroidCarousel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace AndroidCarousel
+{
+	public class MainViewModel : INotifyPropertyChanged
+	{
+		ObservableCollection<string> _listData;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public MainViewModel()
+		{
+			ListData = new ObservableCollection<string> {
+				"0", "1", "2", "3", "4", "5"
+			};
+		}
+
+		public ObservableCollection<string> ListData {
+			get => _listData;
+			set {
+				_listData = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ListData)));
+			}
+		}
+	}
+}
